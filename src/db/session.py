e34d92f662a25e531fc78372e761d087b26d77c7
MaksimFelchuck <felchuck@yandex.ru>
2025-07-21 18:35:14 +0700

    fix

--- src/db/session.py
+++ src/db/session.py
@@ -8,6 +8,13 @@ DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql+psycopg2://felchat:felchat@localhost:5432/felchat"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=60,
+    pool_recycle=3600,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
